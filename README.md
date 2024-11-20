87d2a3a02338f01901ed9cccc34c560ec3808d20
Arianto Zaki Hamdalah <arianto.zaki.hamdalah.03@gmail.com>
2024-11-20 10:15:43 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# Responsive
+
+A Flutter package to make your app responsive according to a reference resolution device or size percentage from the real device.
+
+## Usage
+
+### Initialization
+
+To initialize the `Responsive` class, you need to provide the `BuildContext` and the reference width and height of your design.
+
+```dart
+@override
+void initState() {
+  Responsive.init(context, referenceWidth: 1080, referenceHeight: 1920);
+  super.initState();
+}
+```
+
+### Getting Responsive Sizes
+
+You can get responsive sizes for width and height using the `w` and `h` methods respectively.
+
+```dart
+double responsiveWidth = Responsive.w(100); // Get responsive width for 100 units
+double responsiveHeight = Responsive.h(100); // Get responsive height for 100 units
+```
+
+### Using Extensions
+
+You can also use the provided extensions on `num` to get responsive sizes more conveniently.
+
+```dart
+double responsiveWidth = 100.w; // Get responsive width for 100 units
+double responsiveHeight = 100.h; // Get responsive height for 100 units
+```
+
+### Getting Screen Dimensions
+
+You can get the screen width and height using the `wp` and `hp` properties respectively.
+
+```dart
+double screenWidth = Responsive.wp; // Get screen width
+double screenHeight = Responsive.hp; // Get screen height
+```
+
+### Example
+
+Here is a complete example of how to use the `Responsive` package in your Flutter app:
+
+```dart
+import 'package:flutter/material.dart';
+import 'package:responsive/responsive.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Responsive.init(context, referenceWidth: 1080, referenceHeight: 1920);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Responsive Example')),
+        body: Center(
+          child: Container(
+            width: 100.w, // Responsive width
+            height: 100.h, // Responsive height
+            color: Colors.blue,
+            child: Center(child: Text('Responsive Box')),
+          ),
+        ),
+      ),
+    );
+  }
+}
+```
---
+++ lib/responsive.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class Responsive {
+  late Responsive _initResponsive;
+
+  static late BuildContext _context;
+
+  static late double _referenceWidth;
+  static late double _referenceHeight;
+
+  static late double _screenWidth;
+  static late double _screenHeight;
+
+  factory Responsive(BuildContext context, double referenceWidth, double referenceHeight) =>
+      Responsive._internal(context, referenceWidth, referenceHeight);
+
+  Responsive._internal(BuildContext context, double referenceWidth, double referenceHeight) {
+    _context = context;
+    _referenceWidth = referenceWidth;
+    _referenceHeight = referenceHeight;
+    _setAttributes();
+    _init();
+  }
+
+  void _init() async => _initResponsive = this;
+
+  static Responsive init(BuildContext context, {double referenceWidth = 1080, double referenceHeight = 1920}) {
+    WidgetsFlutterBinding.ensureInitialized();
+    return Responsive(context, referenceWidth, referenceHeight)._initResponsive;
+  }
+
+  void _setAttributes() {
+    final physicalSize = MediaQuery.of(_context).size;
+
+    _screenWidth = physicalSize.width;
+    _screenHeight = physicalSize.height;
+  }
+
+  static double _getResponsiveSize(double size, {required bool isWidth}) {
+    final referenceSize = isWidth ? _referenceWidth : _referenceHeight;
+    final screenSize = isWidth ? _screenWidth : _screenHeight;
+
+    final referenceAspectRatio = _referenceWidth / _referenceHeight;
+    final screenAspectRatio = _screenWidth / _screenHeight;
+
+    if (screenAspectRatio > referenceAspectRatio && _screenHeight > _referenceHeight) {
+      final widthRatio = _screenWidth / _referenceWidth;
+      final heightRatio = _screenHeight / _referenceHeight;
+
+      return size * (widthRatio < heightRatio ? widthRatio : heightRatio);
+    }
+
+    if (screenAspectRatio > referenceAspectRatio) {
+      return size * (screenSize / referenceSize) * (referenceAspectRatio / screenAspectRatio);
+    }
+
+    if (screenAspectRatio < referenceAspectRatio) {
+      return size * (screenSize / referenceSize) * (screenAspectRatio / referenceAspectRatio);
+    }
+
+    return size * (screenSize / referenceSize);
+  }
+
+  static double w(double size) => _getResponsiveSize(size, isWidth: true);
+  static double h(double size) => _getResponsiveSize(size, isWidth: false);
+
+  static double get wp => _screenWidth;
+  static double get hp => _screenHeight;
+}
+
+extension ResponsiveSize on num {
+  double get h => Responsive.h(toDouble());
+  double get w => Responsive.w(toDouble());
+
+  double get hp => Responsive.hp;
+  double get wp => Responsive.wp;
+}
